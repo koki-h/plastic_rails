300b69605583b0f8e5facd0b6e648613df13f6fc
Koki Hashimoto <gohannnotomo@gmail.com>
2020-05-28 15:14:58 +0900

    実行コマンドを色付き（cyan;bold）で出力するようにした。

--- tmpl/setup.sh
+++ tmpl/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -eu
+ESC="\e["
+ESCEND=m
+COLOR_OFF=$(printf ${ESC}${ESCEND})
+COLOR_CMD=$(printf "${ESC}36;1;${ESCEND}") # front: cyan;bold 
 RUN_ON_WEB_CMD="docker-compose exec web"
 #RUN_ON_WEB_CMD="docker-compose run --rm web"
 function docker_run {
   cmd=$1
-  echo $cmd
+  echo ${COLOR_CMD}${cmd}${COLOR_OFF}
   $RUN_ON_WEB_CMD $cmd
 }
 docker-compose up -d
