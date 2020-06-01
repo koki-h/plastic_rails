1d993c9095d59f91b7bd425f0bc9b4b4cd60e23a
Koki Hashimoto <gohannnotomo@gmail.com>
2020-06-01 15:01:08 +0900

    Thorを使ってコマンドの説明等が自動生成されるようにした。

--- plarails.rb
+++ plarails.rb
@@ -1,16 +1,25 @@
 #!/usr/bin/env ruby
 
 $LOAD_PATH << File.dirname(File.expand_path(__FILE__))
+require "thor"
 require "lib/fileutils.rb"
 
-def execute(cmd)
-  puts cmd
-  system(cmd)
+class PlaRails < Thor
+  desc "new APPNAME", "create a Rails application skelton with Docker container."
+  def new(appname)
+    execute("cp -a tmpl #{appname}")
+    Dir.chdir(appname)
+    execute("./build.sh #{appname}")
+    FileUtils.sed("docker-compose.yml",/(working_dir: \/apps\/)/,'\1' + appname) # working_dirをrailsアプリのディレクトリに変更する
+    system("./setup.sh")
+  end
+
+  private
+
+  def execute(cmd)
+    puts cmd
+    system(cmd)
+  end
 end
 
-APPNAME=ARGV[0]
-execute("cp -a tmpl #{APPNAME}")
-Dir.chdir(APPNAME)
-execute("./build.sh #{APPNAME}")
-FileUtils.sed("docker-compose.yml",/(working_dir: \/apps\/)/,'\1' + APPNAME) # working_dirをrailsアプリのディレクトリに変更する
-system("./setup.sh")
+PlaRails.start(ARGV)
