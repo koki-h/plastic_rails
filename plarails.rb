d1d02b65932e3a093f7299d80558bc87c513daea
Koki Hashimoto <gohannnotomo@gmail.com>
2020-06-02 18:06:51 +0900

    ホスト側のDBディレクトリを任意の位置に変更できるようにした #6
    `db_path` オプションでDBディレクトリを指定できるようにした。

--- plarails.rb
+++ plarails.rb
@@ -6,11 +6,21 @@ require "lib/fileutils.rb"
 
 class PlaRails < Thor
   desc "new APPNAME", "create a Rails application skelton with Docker container."
+  option :db_path, :default => "./db/mysql/volumes"
   def new(appname)
     execute("cp -a tmpl #{appname}")
     Dir.chdir(appname)
+
+    # DBのファイルパスを設定する
+    FileUtils.sed("docker-compose.yml",/%DB_PATH%/,options[:db_path]) 
+
+    # Dockerイメージビルド＆rails new (working_dirは `/apps`)
     execute("./build.sh #{appname}")
-    FileUtils.sed("docker-compose.yml",/(working_dir: \/apps\/)/,'\1' + appname) # working_dirをrailsアプリのディレクトリに変更する
+
+    # working_dirをrailsアプリのディレクトリに変更する
+    FileUtils.sed("docker-compose.yml",/(working_dir: \/apps\/)/,'\1' + appname) 
+  
+    # Railsアプリの設定（`bundle install`, `rails db:setup` など）
     system("./setup.sh")
   end
 
