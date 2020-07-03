86e4f61f8dc3e217c2db2b7e8bb5b1dc283ea2a4
Koki Hashimoto <gohannnotomo@gmail.com>
2020-07-03 13:36:37 +0900

    内部的なファイル操作などにThor::Actions のメソッドを使うようにした。 (#14)
    * 外部コマンドの呼び出しに `Thor::Actions#run` を使うようにした。
    
    * 外部コマンドの呼び出しに `Thor::Actions#run` を使うようにした。
    
    修正もれ分
    
    * 不要なオプションを削除した
    
    * ファイル文字列の置換を `Thor::Actions#gsub_file` で行うようにした。
    
    * ファイルコピーを `Thor::Actions#directory` で行うようにした。

--- lib/plastic_rails.rb
+++ lib/plastic_rails.rb
@@ -1,13 +1,17 @@
 require "thor"
 require "plastic_rails/version"
-require "plastic_rails/fileutils"
-  THOR_SILENCE_DEPRECATION = true
+THOR_SILENCE_DEPRECATION = true
 
 module PlasticRails
   class Error < StandardError; end
 
   class PlaRails < Thor
+    include Thor::Actions
     DEFAULT_TEMPLATE_DIR = File.join(File.dirname(__FILE__) , "tmpl")
+
+    def self.source_root
+      Dir.pwd
+    end
 
     def self.exit_on_failure?
       # コマンドが失敗したときに終了ステータス１を返すようにする設定
@@ -18,52 +22,45 @@ module PlasticRails
     option :db_path, :default => "./db/mysql/volumes"
     option :template, :default => DEFAULT_TEMPLATE_DIR
     def new(appname)
-      execute("cp -a #{options[:template]} #{appname}")
-      Dir.chdir(appname)
+      directory(options[:template], appname, :mode => :preserve)
+      inside(appname) do
+        # DBのファイルパスを設定する
+        gsub_file("docker-compose.yml", /%DB_PATH%/, options[:db_path]) 
 
-      # DBのファイルパスを設定する
-      FileUtils.sed("docker-compose.yml",/%DB_PATH%/,options[:db_path]) 
+        # Dockerイメージビルド＆rails new (working_dirは `/apps`)
+        run("./build.sh #{appname}")
 
-      # Dockerイメージビルド＆rails new (working_dirは `/apps`)
-      execute("./build.sh #{appname}")
+        # working_dirをrailsアプリのディレクトリに変更する
+        gsub_file("docker-compose.yml", /(working_dir: \/apps\/)/, '\1' + appname) 
 
-      # working_dirをrailsアプリのディレクトリに変更する
-      FileUtils.sed("docker-compose.yml",/(working_dir: \/apps\/)/,'\1' + appname) 
-
-      # Railsアプリの設定（`bundle install`, `rails db:setup` など）
-      system("./setup.sh")
+        # Railsアプリの設定（`bundle install`, `rails db:setup` など）
+        run("./setup.sh")
+      end
     end
 
     desc "login", "Log in Rails container related to current directory."
     def login
-      execute("docker-compose exec web bash")
+      run("docker-compose exec web bash")
     end
 
     desc "up", "Start up Rails container related to current directory."
     def up
-      execute("docker-compose up -d")
+      run("docker-compose up -d")
     end
 
     desc "stop", "Stop Rails container related to current directory."
     def stop
-      execute("docker-compose stop")
+      run("docker-compose stop")
     end
 
     desc "down", "Stop and remove Rails container related to current directory."
     def down
-      execute("docker-compose down")
+      run("docker-compose down")
     end
 
     desc "copy_template [DEST_DIR]", "Copy the default template to any dir. (To use in `new` command with `--template` option.)"
     def copy_template(dest_dir)
-      execute("cp -a #{DEFAULT_TEMPLATE_DIR} #{dest_dir}")
-    end
-
-    private
-
-    def execute(cmd)
-      puts cmd
-      system(cmd)
+      directory(DEFAULT_TEMPLATE_DIR, dest_dir, :mode => :preserve)
     end
   end
 end
